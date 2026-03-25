89c9cc910394202b1da3d1dcab58504f164cbdd7
joaocastro <joaocastro0429@gmail.com>
2026-03-25 18:46:33 -0300

    add sqlalchemy

--- src/app.py
+++ src/app.py
@@ -1,12 +1,30 @@
+import click
+from flask import Flask, current_app
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
 import os
-from flask import Flask
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
+@click.command('init-db')
+def init_db_command():
+    global db
+    with current_app.app_context():
+     db.create_all()
+    """Clear the existing data and create new tables."""
+    click.echo('Initialized the database.')
+
+
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'dioback.sqlite'),
+      SQLALCHEMY_DATABASE_URI="sqlite:///dio_bank.sqlite",
     )
 
     if test_config is None:
@@ -24,8 +42,9 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
+    app.cli.add_command(init_db_command)
+
     # initialize database
-    from . import db
     db.init_app(app)
 
     return app
