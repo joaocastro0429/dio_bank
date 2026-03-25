a7078af91ad24f2e6d73ea1caf8f2afb1ff1530c
joaocastro <joaocastro0429@gmail.com>
2026-03-25 17:00:50 -0300

    atualizando  rotas

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -6,5 +6,22 @@ app = Flask(__name__)
 def home():
     return "Hello World"
 
-if "__name__" == "__main__":
+@app.route("/sobre")
+def sobre():
+    return "Página sobre"
+
+@app.route("/usuario/<int:id>")
+def usuario(id):
+    return {
+        "usuario":"id"
+    }
+
+
+with app.test_request_context():
+    print(url_for('home'))
+    print(url_for('sobre'))
+    print(url_for('home', next='/'))
+    print(url_for('usuario', id=1))
+
+if __name__ == "__main__":
     app.run(debug=True)
